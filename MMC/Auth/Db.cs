edca5ae1611ac7afff542679a48d3210318eb7ff
Ronny <rmata@itcoint.com>
2022-04-04 23:42:19 -0600

    Commit

---
+++ MMC/Auth/Db.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace MMC.Auth
+{
+    public class Db
+    {
+        // Prepara la conexión
+        private MySqlConnection conexion = new MySqlConnection();
+
+        public Db()
+        {
+            Initialize();
+        }
+        //Initialize values
+        private void Initialize()
+        {
+            conexion = new MySqlConnection(ConfigurationManager.ConnectionStrings["SQL_AUT"].ToString());
+        }
+        public MySqlConnection ObtenerConexion()
+        {
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+
+                return conexion;
+            }
+        }
+        public bool DescargarConexion()
+        {
+            conexion.Close();
+            return true;
+        }
+
+
+    }
+}
---
+++ MMC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MMC.MvcApplication" Language="C#" %>
